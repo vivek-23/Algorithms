ce68e3bfb26a6d140b76c2f8d5dc1cd370f9099d
Vivek Tadpatri <vivekpisces23@gmail.com>
2017-06-02 18:07:04 +0530

    SubarraySumEqualsK.java
    Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
    
    Example 1:
    Input:nums = [1,1,1], k = 2
    Output: 2
    Note:
    The length of the array is in range [1, 20,000].
    The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int sum = 0,cnt = 0;
+        map.put(0,1);
+    
+        
+        for(int i=0;i<nums.length;++i)
+        {
+            sum += nums[i];
+            if(map.containsKey(sum-k)) cnt += map.get(sum-k);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        
+        return cnt;
+    }
+}
